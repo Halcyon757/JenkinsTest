5afe8cd92292a9989b04c067ccb7178ab979db5a
Halcyon757 <employer.videomaker@gmail.com>
2024-11-11 20:48:41 +0300

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/hello")
+@WebServlet("/hello") // Это может быть не обязательно, если вы настраиваете через web.xml
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
