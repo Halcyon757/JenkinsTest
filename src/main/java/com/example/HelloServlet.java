1f171928b98f650c085114b6d194494dd1834d03
Halcyon757 <employer.videomaker@gmail.com>
2024-11-11 22:39:47 +0300

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -10,8 +10,7 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("FDVJOKFNVDJNKFNSDKNFKLDSNFKNKSNVDKFNKSNKJDSNKVNFDSJKNVFKJSDNVNFDSNFVNNFJV"); // Лог для отладки
         resp.setContentType("text/html");
-        resp.getWriter().println("<h1>Hello World!</h1>");
+        resp.getWriter().println("<h1>Hello World! Проверка процесса</h1>");
     }
 }
